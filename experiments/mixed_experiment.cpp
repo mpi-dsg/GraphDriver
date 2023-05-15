87c7214c0ae77f05275aa36ca0dc270aefc05230
Hardik Katehara <hkatehar@mpi-sws.org>
2023-05-15 15:52:33 +0200

    Bind algorithms to one core

--- experiments/mixed_experiment.cpp
+++ experiments/mixed_experiment.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <omp.h>
 
 #include "mixed_experiment.hpp"
 #include "algorithms_experiment.hpp"
@@ -7,24 +8,26 @@
 using namespace std::chrono;
 
 void MixedExperiment::execute() {
+    omp_set_num_threads(configuration().get_n_threads() + 1); // 1 for algorithms + others for updates
     #pragma omp parallel
     {
-        #pragma omp sections
-        {
-            #pragma omp section
-            {
-                UpdatesExperiment experiment {driver, update_stream};
-                experiment.execute();
-            }
+        int threadId = omp_get_thread_num();
 
-            #pragma omp section
-            {
-                
-                AlgorithmsExperiment experiment {driver};
-                experiment.execute();
-            }
+        // Bind thread 0 (function1) to core 0
+        if (threadId == 0) {
+            cpu_set_t cpuset;
+            CPU_ZERO(&cpuset);
+            CPU_SET(0, &cpuset);
+            sched_setaffinity(0, sizeof(cpuset), &cpuset);
+            
+            AlgorithmsExperiment experiment {driver};
+            experiment.execute();
+        }
+
+        #pragma omp single
+        {
+            UpdatesExperiment experiment {driver, update_stream};
+            experiment.execute();
         }
     }
-
-    
 }
